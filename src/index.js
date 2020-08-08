04b378169a4ef07a56e9bb9a10dd9eb993d1d7b4
Angel <jeal.code47@gmail.com>
2020-08-08 18:40:43 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const fetch = require("node-fetch");
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  enum CharacterStatus {
+    Alive
+    Dead
+    unknown
+  }
+
+  type Character {
+    name: String
+    id: ID
+    status: CharacterStatus
+    episodes: [String]
+  }
+
+  type Query {
+    characters: [Character]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    characters: () => fetchCharacters()
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+function fetchEpisodes() {
+  return fetch("https://rickandmortyapi.com/api/episode/")
+    .then((res) => res.json())
+    .then((json) => json.results);
+}
+
+function fetchEpisodeById(id) {
+  return fetch("https://rickandmortyapi.com/api/episode/" + id)
+    .then((res) => res.json())
+    .then((json) => json);
+}
+
+function fetchEpisodeByUrl(url) {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((json) => json);
+}
+
+function fetchCharacters() {
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then((res) => res.json())
+    .then((json) => json.results);
+}
+
+function fetchCharacterById(id) {
+  return fetch("https://rickandmortyapi.com/api/character/" + id)
+    .then((res) => res.json())
+    .then((json) => json);
+}
+
+function fetchCharacterByUrl(url) {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((json) => json);
+}
